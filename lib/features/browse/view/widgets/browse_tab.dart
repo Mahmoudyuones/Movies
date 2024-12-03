ce87852e8fe604f9b48645b36039439810e1f9fe
Mahmoudyuones <mywns211@gmail.com>
2024-12-03 21:06:16 +0200

    working on Browse ui

--- lib/features/browse/view/widgets/browse_tab.dart
+++ lib/features/browse/view/widgets/browse_tab.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:movies/features/browse/view/widgets/borwse_category_item.dart';
 
 class BrowseTab extends StatelessWidget {
   const BrowseTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'this is browse',
-        style: TextStyle(color: Colors.red),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
+            child: Text(
+              'Browse Category ',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(fontSize: 22),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(10),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+              itemBuilder: (_, index) => const BorwseCategoryItem(
+                  categoryName: 'Action', imageName: 'category_item'),
+              itemCount: 20,
+            ),
+          )
+        ],
       ),
     );
   }
