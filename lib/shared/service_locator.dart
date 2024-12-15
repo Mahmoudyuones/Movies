d6ae276bb63ab8f18da694516d18f60e8633880e
Mahmoudyuones <mywns211@gmail.com>
2024-12-15 02:59:15 +0200

    edit service_locator

--- lib/shared/service_locator.dart
+++ lib/shared/service_locator.dart
@@ -8,7 +8,7 @@ import 'package:movies/features/home/data/data_source/top_rated_movies/top_rated
 import 'package:movies/features/home/data/data_source/top_rated_movies/top_rated_data_source.dart';
 import 'package:movies/features/home/data/data_source/upcoming_movies/upcomimg_data_source.dart';
 import 'package:movies/features/home/data/data_source/upcoming_movies/upcoming_api_data_source.dart';
-import 'package:movies/features/movie_details/data/data_source/movie_details_API_data_source.dart';
+import 'package:movies/features/movie_details/data/data_source/movie_details_api_data_source.dart';
 import 'package:movies/features/movie_details/data/data_source/movie_details_data_source.dart';
 import 'package:movies/features/search/data/data_source/search_api_data_source.dart';
 import 'package:movies/features/search/data/data_source/search_data_source.dart';
