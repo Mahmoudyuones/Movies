05a105fab9945356e9615407317b7d600c8a3c44
shehab358 <shehabyusuf12@gmail.com>
2024-12-17 18:33:59 +0200

    edited categoty item defult image

--- lib/features/browse/view/widgets/borwse_category_item.dart
+++ lib/features/browse/view/widgets/borwse_category_item.dart
@@ -1,33 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:movies/shared/app_theme/app_colors.dart';
 
+// ignore: must_be_immutable
 class BorwseCategoryItem extends StatelessWidget {
-  const BorwseCategoryItem(
+  BorwseCategoryItem(
       {super.key, required this.imageName, required this.categoryName});
   final String imageName;
   final String categoryName;
+  List<String> imageNames = [
+    'Action',
+    'Animation',
+    'Comedy',
+    'Drama',
+    'Fantasy',
+    'Music',
+    'Romance',
+    'War'
+  ];
+
   @override
   Widget build(BuildContext context) {
+    bool isInList = imageNames.contains(imageName);
+
     return Padding(
       padding: const EdgeInsets.only(bottom: 10, top: 10),
       child: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage(
-              'assets/images/$imageName.webp',
-            ),
+            image: AssetImage(isInList
+                ? 'assets/images/$imageName.webp'
+                : 'assets/images/category_item.png'),
             fit: BoxFit.fill,
             colorFilter: ColorFilter.mode(
-                AppColors.primary.withOpacity(0.2), BlendMode.multiply),
+              AppColors.primary.withOpacity(0.2),
+              BlendMode.multiply,
+            ),
           ),
         ),
         child: Center(
           child: Text(
             categoryName,
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge!
-                .copyWith(fontWeight: FontWeight.w600),
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
           ),
         ),
       ),
